53305f0e6cc1af37d5f7653ad947b1663e46fae2
John Maxwell <jgwmaxwell@gmail.com>
2026-02-14 14:31:07 +0100

    fix: change dependency commit

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/tripleclabs/swift-distributed-actors.git", branch: "main")
+        .package(url: "https://github.com/tripleclabs/swift-distributed-actors.git", revision: "27b33b38b0e59f9921c2db344dbbac5454a9f313")
     ],
     targets: [
         .target(
